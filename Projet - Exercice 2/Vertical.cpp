80741684782b1a918521a59710b4dad1aa23a210
arnaud deffert <2002702@cstj.qc.ca>
2020-09-11 12:20:29 -0400

    remise sur github

---
+++ Projet - Exercice 2/Vertical.cpp
@@ -0,0 +1,65 @@
+// But : Le programme à comme entrée un nombre de 5 chiffres, il verifie que celui-ci comprend bien 5 chiffres et l'affiche verticalement
+//Auteur : DEFFERT Arnaud
+// Date : 200-09-14
+
+#include <iostream> // Pour permettre au programme d'interagir avec l'utilisateur cout, cin, 
+
+int main()
+
+{
+	setlocale(LC_ALL, "");  // fait apparaitre correctement les accents de windows à la console
+
+	// au debut on déclare les valeurs
+
+	int nombre; // déclarer un nombre entier
+
+std::cout << "Saisir un nombre à 5 chiffres :"; //On invite l'utilisateur à saisir un nombre
+std::cin >> nombre; // Le programme lit et enregistre la valeur
+
+//si le nombre est inférieur à 10000 il affiche Ce nombre ne contient pas 5 chiffres.
+
+if (nombre < 10000)
+{
+	std::cout << "Ce nombre ne contient pas 5 chiffres.";
+
+}
+
+//si le nombre est  supérieur ou égale à 10000  et inférieur ou égale à 99999 il a 5 chiffres
+if (nombre >= 1000)
+if (nombre <=99999)
+
+
+{// bloc instruction exécuté si la condition est vraie
+
+	std::cout << nombre / 10000 % 10; //l'opérateur demande de diviser le nombre par 10000 modulo 10 pour afficher des dizaine de 1000
+	std::cout << nombre / 1000 % 10 ; //l'opérateur demande de diviser le nombre par 1000 modulo 10 pour afficher l'unite de 1000
+	std::cout << nombre / 100 % 10; //l'opérateur demande de diviser le nombre par 100 modulo 10pour afficher l'unité de 100
+	std::cout << nombre / 10 % 10; //l'opérateur demande de diviser le nombre par 10 modulo 10pour afficher le chiffre des dizaine
+	std::cout << nombre  %10; //l'opérateur demande de diviser le nombre par  modulo 10 pour afficher le nombre des unités
+
+}
+else
+{ //bloc instruction exécuté si la condition est fausse
+
+	std::cout << "Ce nombre ne contient pas 5 chiffres.";
+
+}
+
+
+}
+// plan de test
+
+/*
+nombre				resultat souhaite									erreur
+12345				12345 verticalement						affiche sur la même ligne
+																	12345
+																		
+													
+													
+
+123				Ce nombre ne contient pas 5 chiffres.
+10000															affiche sur la même ligne 10000
+123456			Ce nombre ne contient pas 5 chiffres.								
+
+
+*/
--- Projet-Exercice 1/PairImpair.cpp
+++ Projet-Exercice 1/PairImpair.cpp
@@ -6,25 +6,40 @@
 
 
 
-int x;
-main()
-
+int main()
 {
 	setlocale(LC_ALL, "");  // fait apparaitre correctement les accents de windows à la console
 
-	cout <<"Donnez un nombre :"; //l'utilisateur donne un nombre
-	cin >> x;
-	if (x % 2 == 0)
-		cout << "Le nombre est pair"; //Le programme répond si le chiffre est pair
-	}else{
 
-	cout << "Le nombre est impair"; //Le programme répond si le chiffre est impair
+	int nombre; // déclarer nombre entier
+	
+	std::cout <<"Saisir un nombre :"; //On invite l'utilisateur à saisir un nombre
 
+	std::cin >> nombre; // Le programme lit et enregistre la valeur
+
+
+	if (nombre % 2 == 0) // mise en place de la condition qui vérifie si le nombre est pair ou impair
+	{ 
+		std::cout << nombre << " est un nombre pair"; //Le programme affiche que le nombre taper par l'utilisateur est pair.
+	}
+
+	else
+	{
+		 std::cout << nombre << " est un nombre impair"; //Le programme affiche que le nombre taper par l'utilisateur  est impair
+	}
 
 	return 0;
 
-
-
 }
+
+// Plan de test
+
+/*
+nombre			resultat
+1				impair
+12				pair
+15				impair
+0				pair
+*/
 
 
