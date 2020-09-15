497382b8ba827bb4888fa3c9d3a57c92f0a611a8
Karine Moreau <70488225+kmoreau-cstj@users.noreply.github.com>
2020-09-15 14:04:21 -0400

    Parfait !
    Très bon travail et code bien commenté !

--- Projet-Exercice 1/PairImpair.cpp
+++ Projet-Exercice 1/PairImpair.cpp
@@ -1,26 +1,26 @@
-// But : Le programme doit lire un Entier, Déterminer et Afficher s'il s'agit d'un entier Pair ou Impair
+// But : Le programme doit lire un Entier, DÃ©terminer et Afficher s'il s'agit d'un entier Pair ou Impair
 // Auteur :  DEFFERT Arnaud
 // Date : 2020-09-14
 
-#include <iostream> // boite à outils pour communiquer avec l'utilisateur, cout cin
+#include <iostream> // boite Ã  outils pour communiquer avec l'utilisateur, cout cin
 
 
 
 int main()
 {
-	setlocale(LC_ALL, "");  // fait apparaitre correctement les accents de windows à la console
+	setlocale(LC_ALL, "");  // fait apparaitre correctement les accents de windows Ã  la console
 
 
-	int nombre; // déclarer nombre entier
+	int nombre; // dÃ©clarer nombre entier
 	
-	std::cout <<"Saisir un nombre :"; //On invite l'utilisateur à saisir un nombre
+	std::cout <<"Saisir un nombre :"; //On invite l'utilisateur Ã  saisir un nombre
 
 	std::cin >> nombre; // Le programme lit et enregistre la valeur
 
 
-	if (nombre % 2 == 0) // mise en place de la condition qui vérifie si le nombre est pair ou impair
+	if (nombre % 2 == 0) // mise en place de la condition qui vÃ©rifie si le nombre est pair ou impair
 	{ 
-		std::cout << nombre << " est un nombre pair"; //Le programme affiche que le nombre taper par l'utilisateur est pair.
+		std::cout << nombre << " est un nombre pair"; //FR : Le programme affiche que le nombre taper par l'utilisateur est pair.
 	}
 
 	else
